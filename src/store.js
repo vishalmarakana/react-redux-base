d0578227423fdfdca7614e7861147f8fdee90025
Vishal Marakana <vishal.marakana@gmail.com>
2018-04-19 15:36:08 +0530

    redux developer tool update

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 const initialState = {};
@@ -8,9 +9,8 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer, 
     initialState, 
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+        applyMiddleware(...middleware)
     )
 );
 
